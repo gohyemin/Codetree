ff45b8a0b903de2536bc1ae95aa9935a87c4a419
hemkong2 <gohm9464@naver.com>
2025-05-06 16:55:15 +0900

    Title: 점수 입출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250506/점수 입출력/enter-int-and-print-score.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N;
+    cin >> N;
+    cout << "Your score is " << N;
+    cout << " point.";
+
+    return 0;
+}
